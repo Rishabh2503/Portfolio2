cbea61f9f04c3b15134b202b5144f9405f795af7
Yash Mishra <praisedboy5000@gmail.com>
2024-06-01 22:34:12 +0530

    changes done

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,10 +16,10 @@ const about = () => {
         <meta name='description' content='About me' />
       </Head>
       <main className='flex flex-col items-center justify-center w-full dark:text-light'>
-  <Layout className='pt-16'>
+  <Layout className='pt-16 md:pt-0'>
     <Animatedtext
       text='Innovation Drives My Web Development.'
-      className='mb-16 text-center' />
+      className='mb-16 text-center md:md-0 sm:mt-5' />
     <div className="grid w-full grid-cols-8 gap-16 lg:flex lg:flex-col lg:grid-cols-1">
       <div className='flex flex-col items-start justify-start col-span-3 lg:col-span-8'>
         <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>BioGraphy</h2>
@@ -36,7 +36,7 @@ const about = () => {
         </p>
       </div>
 
-      <div className='relative self-center col-span-3 p-8 border-2 border-solid h-[400px] lg:w-[40vw] rounded-2xl border-dark bg-light dark:border-light dark:bg-dark lg:col-span-8'>
+      <div className='relative self-center col-span-3 p-8 border-2 border-solid h-[50vh] md:w-[50vw] lg:w-[45vw] w-[30vw] md:h-[40vh] sm:h-[25vh] rounded-2xl border-dark bg-light dark:border-light dark:bg-dark lg:col-span-8'>
         {/* <div className='absolute top-0 -right-3 -z-10 w-[80vw] h-full rounded-[2rem] bg-dark dark:border-light' /> */}
         <div className='flex justify-center h-full'>
           <Image src={Rishabh2} alt="Rishabh" className='w-auto h-full rounded-2xl' />
